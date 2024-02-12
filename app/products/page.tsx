765350edfd30c15729475329b192a635381b093e
nazimuddin <nazimuddin102001@gmail.com>
2024-02-12 13:04:01 +0600

    added dynamic routing

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>Product 1</ul>
+      <ul>Product 2</ul>
+      <ul>Proudct 3</ul>
+    </div>
+  );
+};
+
+export default Products;
